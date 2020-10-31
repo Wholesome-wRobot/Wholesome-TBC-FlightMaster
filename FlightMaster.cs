8d8d765c08b1e759bd0b9c774f35af068968c1bd
Wholesome-wRobot <jeanlouisledaron@gmail.com>
2020-10-31 08:24:15 +0100

    Ajoutez des fichiers projet.

---
+++ FlightMaster.cs
@@ -0,0 +1,20 @@
+﻿using robotManager.Helpful;
+using wManager.Wow.Enums;
+
+public class FlightMaster
+{
+    public int NPCId { get; set; }
+    public Vector3 Position { get; set; }
+    public string Name { get; set; }
+    public ContinentId Continent { get; set; }
+    public bool AlreadyDiscovered { get; set; }
+
+    public FlightMaster(string name, int npcId, Vector3 position, ContinentId continent, bool alreadyDiscovered)
+    {
+        Name = name;
+        NPCId = npcId;
+        Position = position;
+        Continent = continent;
+        AlreadyDiscovered = alreadyDiscovered;
+    }
+}
---
+++ Logger.cs
@@ -0,0 +1,10 @@
+﻿using robotManager.Helpful;
+using System.Drawing;
+
+public class Logger
+{
+    public static void Log(string s)
+    {
+        Logging.Write($"[Wholesome TBC FlightMaster] {s}", Logging.LogType.Normal, Color.DarkCyan);
+    }
+}
